7d790897f3be46c78a485a4f193ba81d53338536
Enrico Rotondo <ENRICO.ROTONDO@local.ittcesena.it>
2023-03-18 09:01:20 +0100

    Versione 1.1: classe Alunno terminata

--- scuolaRecupero/Alunno.cs
+++ scuolaRecupero/Alunno.cs
@@ -8,5 +8,48 @@ namespace scuolaRecupero
 {
     internal class Alunno
     {
+        private string nome;
+        private string cognome;
+        private int eta;
+        private string classe;
+
+
+        public Alunno(string nome, string cognome, int eta, string classe)
+        {
+            this.nome = nome;
+            this.cognome = cognome;
+            this.eta = eta;
+            this.classe = classe;
+        }
+
+
+        public void addAlunno(List<string> lista)
+        {
+            string alunno = nome + cognome + eta.ToString() + classe;
+            lista.Add(alunno);
+            
+        }
+
+        public string toString(string nome, List<string> lista)
+        {
+            for(int i=0; i< lista.Count; i++)
+            {
+                if (nome == lista[i])
+                {
+                    return lista[i];
+                }
+            }
+        }
+
+        public string classi(int anni)
+        {
+            if (anni < 16)
+            {
+                return "Biennio";
+            }
+            else {
+                return "Triennio";
+            }
+        }
     }
 }
